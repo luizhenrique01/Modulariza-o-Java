78ab83d23613fe63b3dcde79bfb967683c157d67
Luiz Henrique Oliveira de Assis <51269270+luizhenrique01@users.noreply.github.com>
2021-05-07 16:39:03 -0300

    Exercicio 8

---
+++ Exercicio Modularizacao 8/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //variaveis
+        Scanner entrada = new Scanner(System.in);
+        int N,S;
+
+        //entrada
+        N = entrada.nextInt();
+
+        //funcao
+        S = Formula(N);
+        if (S > 0) {
+            System.out.println("O valor de N na funcao e: " + S);
+        }else{
+            System.out.println("A funcao nao aceita numeros negativos");
+        }
+    }
+
+    public static int Formula(int N) {
+
+        if (N > 0){
+            return (int)((Math.pow(N,2)+1)/(N+3));
+        } else{
+            return -1;
+        }
+
+    }
+
+}
